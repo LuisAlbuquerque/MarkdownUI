adc93da40871aa15e76b94ce5d1ec0ed050be422
LuisAlbuquerque <lmiguelaa@gmail.com>
2019-08-18 22:17:19 +0100

    add shortcuts

--- app/web/mdjs.js
+++ app/web/mdjs.js
@@ -3,16 +3,43 @@ function send_filejs() {
 	eel.create_file(file)(setImage)
 }
 
-function receive_filejs() {
-	var data = document.getElementById("data2").value
-	var file = document.getElementById("name_file").value
-	eel.receive_file(data,file)(setImage)
+function title_() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "\n#";
 }
 
-function setImage(base64) {
-	document.getElementById("qr").src = base64
+function subtitle() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "\n##";
 }
 
-function set_id(name) {
-        document.getElementById("id_client").innerHTML = "id : " + name;
+function subsubtitle() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "\n###";
 }
+
+function add_image() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "![tag image](path image)"
+}
+
+function add_table() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "|   |   |   |   |   |\n |---|---|---|---|---|\n |   |   |   |   |   |\n |   |   |   |   |   |\n |   |   |   |   |   |"
+}
+
+function new_line() {
+	var text = document.getElementById("file_content").value;
+	document.getElementById("file_content").value = text + "\n \\newline \n";
+}
+
+function add_indice() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "\n \\tableofcontents";
+}
+
+function add_header() {
+	var text = document.getElementById("file_content").value
+	document.getElementById("file_content").value = text + "---\n title: \"\"\n author: [Author Name]\n date: \"\"\n subject: \"Markdown\"\n keywords: [Markdown, Example]\n subtitle: \"\"\n lang: \"en\"\n titlepage: true\n titlepage-color: \"06386e\"\n titlepage-text-color: \"FFFFFF\"\n titlepage-rule-color: \"FFFFFF\"\n titlepage-rule-height: 1\n ...\n";
+}
+
